a0f137334c62e5ddc1f67fa857a7e5f9da625a40
DANIELLY FERREIRA DA SILVA <danielly.fsilva3@senacsp.edu.br>
2026-04-14 09:17:44 -0300

    views

--- sistemafarmacia.sql
+++ sistemafarmacia.sql
@@ -173,16 +173,13 @@ WHERE codproduto = 1;
 SELECT * FROM farmacia;
 
 --Selecionando todos os dados de uma coluna
+
 SELECT nomefarmacia
 FROM farmacia;
-
 --Selecionando todos os dados de duas colunas
 SELECT nomefarmacia, cidade
 FROM farmacia;
 
---Selecionando com filtro
-SELECT * FROM farmacia
-WHERE cidade = 'São Paulo';
 
 --Selecionando com WHERE e AND
 SELECT * FROM farmacia
@@ -295,9 +292,7 @@ ON farmacianova.*
 TO 'usuario_farmacia'@'localhost';
 
 --Concedendo permissões específicas
-GRANT SELECT, INSERT
-ON farmacianova.*
-TO 'usuario_farmacia'@'localhost';
+SELECT * FROM `farmacia` WHERE 1
 
 --Revogando permissões de um usuário
 REVOKE INSERT
@@ -311,3 +306,170 @@ FROM 'usuario_farmacia'@'localhost';
 
 --Excluíndo usuário 
 DROP USER 'usuario_farmacia'@'localhost';
+
+--Procedure: listar produtos de uma farmácia
+DELIMITER $$
+ 
+CREATE PROCEDURE listar_produtos_farmacia (IN cnpjFarm VARCHAR(14))
+BEGIN
+    SELECT codproduto, quantproduto, valorproduto
+    FROM produto
+    WHERE cnpj_farmacia = cnpjFarm;
+END$$
+ 
+DELIMITER ;
+ 
+--executando o procedure
+CALL listar_produtos_farmacia('12345678000199');
+
+ --Procedure: atualizar o valor do produto
+DELIMITER $$
+ 
+CREATE PROCEDURE atualizar_valor_produto (
+    IN p_cod INT,
+    IN p_valor DECIMAL(10,2)
+)
+BEGIN
+    UPDATE produto
+    SET valorproduto = p_valor
+    WHERE codproduto = p_cod;
+END$$
+ 
+DELIMITER ;
+ 
+--Executando o procedure acima
+CALL atualizar_valor_produto(1, 19.90);
+ 
+--Function: calcular o valor total em estoque
+DELIMITER $$
+ 
+CREATE FUNCTION valor_total_produto(p_cod INT)
+RETURNS DECIMAL(10,2)
+DETERMINISTIC
+BEGIN
+    DECLARE total DECIMAL(10,2);
+ 
+    SELECT quantproduto * valorproduto
+    INTO total
+    FROM produto
+    WHERE codproduto = p_cod;
+ 
+    RETURN total;
+END$$
+ 
+DELIMITER ;
+ 
+--usando a função acima
+SELECT valor_total_produto(1) AS total_em_estoque;
+
+--fUNCTION: QUANTIDADE DE PRODUTOS POR FARMACIA
+DELIMITER $$
+
+CREATE FUNCTION qtd_produtos_farmacia(p_cnpj VARCHAR(14))
+RETURNS INT
+DETERMINISTIC
+BEGIN
+DECLARE total INT;
+
+SELECT COUNT(*)
+INTO total
+FROM produto
+WHERE cnpj_farmacia = p_cnpj;
+
+RETURN total;
+END$$
+DELIMITER ;
+
+--Executando a função acima
+DELIMITER $$
+
+DROP FUNCTION IF EXISTS qtd_produtos_farmacia $$
+
+CREATE FUNCTION qtd_produtos_farmacia(p_cnpj VARCHAR(14))
+RETURNS INT 
+DETERMINISTIC
+BEGIN
+    DECLARE total INT; 
+
+    SELECT SUM(quantproduto)
+    INTO total 
+    FROM produto 
+    WHERE cnpj_farmacia = p_cnpj;
+
+    
+    RETURN IFNULL(total, 0);
+END$$
+
+DELIMITER ;
+
+
+--Testando a função acima
+SELECT qtd_produtos_farmacia('12345678000199') AS total_em_estoque;
+
+--Trigger: impedir quantidade negativa (BEFORE INSERT)
+DELIMITER $$
+CREATE TRIGGER trg_before_insert_produto
+BEFORE INSERT ON produto
+FOR EACH ROW
+BEGIN
+IF NEW.quantproduto < 0 THEN
+SET NEW.quantproduto = 0;
+END IF; 
+END$$
+
+DELIMITER ;
+
+--tENTANDO INSERIR UM VALOR
+
+INSERT INTO produto (quantproduto, valorproduto, cnpj_farmacia)
+VALUES (-10, 20.00, '12345678000199');
+
+-- Trigger: impedir valor negativo (BEFORE UPDATE)
+DELIMITER $$
+
+CREATE TRIGGER trg_before_update_produto
+BEFORE UPDATE ON produto
+FOR EACH ROW
+BEGIN
+IF NEW.valorproduto < @ THEN
+SET NEW.valorproduto = OLD.valorproduto;
+END IF;
+END$$
+DELIMITER ;
+
+-- testando o código acima
+UPDATE produto
+SET valorproduto = -25.00
+WHERE codproduto = 1;
+
+
+-- View: farmácia + produto
+CREATE VIEW vw_farmacia_produto AS
+SELECT
+    f.nomefarmacia,
+    f.cidade,
+    p.codproduto,
+    p.quantproduto,
+    p.valorproduto
+FROM farmacia f
+INNER JOIN produto p
+ON f.cnpj = p.cnpj_farmacia;
+
+-- testando o código acima
+SELECT * FROM vw_farmacia_produto;
+
+--View: farmácia + farmaceutico
+CREATE VIEW vw_farmaceutico_farmacia AS
+SELECT
+    fa.nomefarmacia,
+    fa.cidade,
+    fm.nomefarmaceutico 
+FROM farmacia fa
+INNER JOIN farmaceutico fm
+    ON fa.cnpj = fm.cnpj_farmacia;
+
+--testando o codigo acima
+CREATE VIEW vw_produtos_caros AS 
+SELECT * 
+FROM produto 
+WHERE valorproduto > 30;
